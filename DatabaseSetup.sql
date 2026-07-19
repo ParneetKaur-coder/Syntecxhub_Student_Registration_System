5d7fb0d9685809e1fc87464f47a7d12371670e63
Parneet kaur <p80156433@gmail.com>
2026-07-19 12:17:58 +0530

    Add database schema

---
+++ DatabaseSetup.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE studentdb;
+
+USE studentdb;
+
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    course VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
